35cc829455690b17e652d3dc0973b0c686a5b4d7
Jokerblazes <zcd123@qq.com>
2018-04-17 16:04:23 +0800

    完成以卫语句替换条件表达式的练习

--- src/main/java/practice1/Security.java
+++ src/main/java/practice1/Security.java
@@ -11,28 +11,10 @@ public class Security {
     }
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
-
-        boolean isAccess = false;
-        if (user == null) {
-            return isAccess;
-        }
-
-        if (permission == null) {
-            return isAccess;
-        }
-
-        if (permissions.size() == 0) {
-            return isAccess;
-        }
-
-        if (securityChecker.isAdmin()) {
-            isAccess = true;
-        }
-
-        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
-            isAccess = true;
-        }
-
-        return isAccess;
+        if (securityChecker.isAdmin())
+            return true;
+        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission))
+            return true;
+        return false;
     }
 }
