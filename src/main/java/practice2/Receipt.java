4a3758ba75b4b004b7e97f71366d9dc3fd24c9f0
Jokerblazes <zcd123@qq.com>
2018-04-17 16:34:28 +0800

    完成使用提炼函数的练习

--- src/main/java/practice2/Receipt.java
+++ src/main/java/practice2/Receipt.java
@@ -14,7 +14,17 @@ public class Receipt {
 
     public double CalculateGrandTotal(List<Product> products, List<OrderItem> items) {
         BigDecimal subTotal = calculateSubtotal(products, items);
+        subTotal = calculateReducedPrice(products, items, subTotal);
+        BigDecimal grandTotal = calculateTaxTotal(subTotal);
+        return grandTotal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
 
+    private BigDecimal calculateTaxTotal(BigDecimal subTotal) {
+        BigDecimal taxTotal = subTotal.multiply(tax);
+        return subTotal.add(taxTotal);
+    }
+
+    private BigDecimal calculateReducedPrice(List<Product> products, List<OrderItem> items, BigDecimal subTotal) {
         for (Product product : products) {
             OrderItem curItem = findOrderItemByProduct(items, product);
 
@@ -24,10 +34,7 @@ public class Receipt {
 
             subTotal = subTotal.subtract(reducedPrice);
         }
-        BigDecimal taxTotal = subTotal.multiply(tax);
-        BigDecimal grandTotal = subTotal.add(taxTotal);
-
-        return grandTotal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        return subTotal;
     }
 
 
